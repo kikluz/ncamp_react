3e52bf535522633faa631a8b45c59b0f32cebb51
kikluz <savir81@hotmail.com>
2021-06-21 19:21:50 -0700

    daa new file an priject

--- src/App.js
+++ src/App.js
@@ -1,21 +1,33 @@
-import React,{ Component } from 'react';
-import {Navbar, NavbarBrand} from 'reactstrap';
+import React, {Component} from 'react';
+import { Navbar, NavbarBrand } from 'reactstrap';
+import Directory from './components/DirectoryComponent';
 import './App.css';
-
+// import CAMPSITES array to this file
+import {CAMPSITES} from './shared/campsites';
+// set the local state in App.js so we have data from campsites.js inside App.js
 class App extends Component {
-  render() {
-    return (
-      <div className="app">
-        <Navbar dark color="primary">
-          <div className="container">
-            <NavbarBrand href="/">
-              NuCamp
-            </NavbarBrand>
-          </div>
-        </Navbar>
-      </div>
-    );
-  }
-}
+	constructor(props){
+		super(props);
+			this.state = {
+				campsites: CAMPSITES
+			};
+	}
 
+	render(){
+		return (
+			<div className="App">
+
+				<Navbar dark color="primary">
+					<div className="container">
+						<NavbarBrand href="/">NuCamp</NavbarBrand>
+					</div>
+				</Navbar>
+			{/*Passing down as props to the Directory component*/}
+				<Directory campsites={this.state.campsites} />
+
+			</div>
+			);
+	}
+}
+// this export its for this entires file
 export default App;
---
+++ src/components/DirectoryComponent.js
@@ -0,0 +1,47 @@
+import React, { Component } from "react";
+import { Card, CardImg, CardImgOverlay, CardText, CardBody, CardTitle } from 'reactstrap';
+import CampsiteInfo from './CampsiteInfoComponent';
+
+class Directory extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      selectedCampsite: null,
+    };
+  }
+// method for click on
+  onCampSiteSlected(campsite){
+    this.setState({selectedCampsite: campsite});
+  }
+
+
+
+  render() {
+    const directory = this.props.campsites.map(campsite => {
+      return (
+    
+        <div key={campsite.id} className="col-md-5 m-1">
+          <Card onClick={() => this.onCampSiteSlected(campsite)}>
+            <CardImg with='100%' src={campsite.image} alt={campsite.name} />
+            <CardImgOverlay>
+             <CardTitle>{campsite.name}</CardTitle>
+            </CardImgOverlay>
+          </Card>
+        </div>
+        
+        );
+    }); 
+
+    return (
+      <div className="container">
+      <div className="row">
+      {directory}
+      </div>
+     <CampsiteInfo campsite={this.state.selectedCampsite}/>
+      </div>
+      );
+  }
+}
+
+
+export default Directory;
