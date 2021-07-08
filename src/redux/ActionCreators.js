2ddd7c3f8d464c8ab209c519503aa0dbe1610c2e
kikluz <savir81@hotmail.com>
2021-07-07 19:15:32 -0700

    Fetch Handling Errors

--- src/redux/ActionCreators.js
+++ src/redux/ActionCreators.js
@@ -22,11 +22,29 @@ export const fetchCampsites = () => dispatch => {
     dispatch(campsitesLoading());
 
     return fetch(baseUrl + 'campsites')
+    // Adding error handling Errors 
+    .then(response => {
+             if (response.ok) {
+                return response;
+            } else {
+                // Create an error object and throw it, will be caught by the catch block
+                const error = new Error(`Error ${response.status}: ${response.statusText}`);
+                    error.response = response;
+                    throw error;
+            }
+        },
+
+        error => {
+            const errMess = new Error(error.message);
+            throw errMess;
+        }
+    )
     // call a fetch to retirn a promise, use json mathod to convert the response into javascript
         .then(response => response.json())
          // grap  that javascript array in the campsite argument, dispatch that camsite's argument with
         // addCampsites action creator to be used as a payload
-        .then(campsites => dispatch(addCampsites(campsites)));
+        .then(campsites => dispatch(addCampsites(campsites)))
+        .catch(error => dispatch(campsitesFailed(error.message)));
 
 };
 
@@ -47,8 +65,26 @@ export const addCampsites = campsites => ({
 // thunk action creator with 2 doble arrow function, dispatch argument passed into the inner arrow function
 export const fetchComments = () => dispatch => {    
     return fetch(baseUrl + 'comments')
+        // Adding error handling Errors 
+        .then(response => {
+            if (response.ok) {
+                return response;
+            } else {
+            // Create an error object and throw it, will be caught by the catch block
+            const error = new Error(`Error ${response.status}: ${response.statusText}`);
+                error.response = response;
+                throw error;
+            }
+        },
+
+        error => {
+            const errMess = new Error(error.message);
+            throw errMess;
+        }
+    )
         .then(response => response.json())
-        .then(comments => dispatch(addComments(comments)));
+        .then(comments => dispatch(addComments(comments)))
+        .catch(error => dispatch(commentsFailed(error.message)));
 };
 
 // setup action creator for handle comments, returns action objects (no redux thunk)
@@ -67,8 +103,26 @@ export const fetchPromotions = () => dispatch => {
     dispatch(promotionsLoading());
 
     return fetch(baseUrl + 'promotions')
+        // Adding error handling Errors 
+        .then(response => {
+            if (response.ok) {
+                return response;
+            } else {
+            // Create an error object and throw it, will be caught by the catch block
+            const error = new Error(`Error ${response.status}: ${response.statusText}`);
+                error.response = response;
+                throw error;
+            }
+        },
+
+        error => {
+            const errMess = new Error(error.message);
+            throw errMess;
+        }
+    )
         .then(response => response.json())
-        .then(promotions => dispatch(addPromotions(promotions)));
+        .then(promotions => dispatch(addPromotions(promotions)))
+        .catch(error => dispatch(promotionsFailed(error.message)));
 };
 
 export const promotionsLoading = () => ({
